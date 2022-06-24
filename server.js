064c8cc7bae10ce26a0d9922e2f7b219803eeb13
FocusKing <104179717+FocusKing@users.noreply.github.com>
2022-06-24 09:50:54 -0400

    staging for homework

---
+++ server.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql2'); // Import and require mysql2
+
+
+
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // MySQL password
+    password: '',
+    database: 'classlist_db'
+  },
+  console.log(`Connected to the classlist_db database.`)
+);
+
+// Query database
+db.query('SELECT * FROM students', function (err, results) {
+  console.table(results);
+});
+
