8e782e66e8143325b9793b86766b4c4e9553d326
FocusKing <104179717+FocusKing@users.noreply.github.com>
2022-07-04 00:44:23 -0400

    reworked with tutor

--- server.js
+++ server.js
@@ -12,6 +12,24 @@ const db = mysql.createConnection(
   // console.log(`Connected to the employees_db database.`)
 );
 
+db.connect(function (err) {
+  if (err) throw err;
+  init();
+});
+
+const allEmployees = () => {
+  db.query('SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id =department.id LEFT JOIN employee manager ON manager.id = employee.manager_id', 
+  function (err, results) {
+    if(err) return console.error(err);
+    console.table(results);
+    init();
+  });
+}
+
+
+
+
+
 const init = () => {
   inquirer.prompt([
     {
