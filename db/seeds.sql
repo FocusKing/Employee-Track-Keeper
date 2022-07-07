984e0c3d21953fa8223b5e915456091edd72e0af
FocusKing <104179717+FocusKing@users.noreply.github.com>
2022-07-06 23:09:11 -0400

    updated seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,27 +5,25 @@ VALUES ("Management"),
        ("Finance"),
        ("Engineering"),
        ("Sales"),
-       ("Coordinator"),
-       ("Customer Service"),
        ("Marketing");
        
 INSERT INTO role (title, salary, department_id)
-VALUES ("VP of Operations", 250000, 1),
-       ("Lead Director", 200000, 2),
-       ("Sales Manager", 175000, 5),
+VALUES ("VP of Operations", 270000, 1),
+       ("Lead Director", 220000, 2),
+       ("Sales Manager", 185000, 5),
        ("Application Engineer", 110000, 4),
-       ("Salesperson", 150000, 4),
+       ("Salesperson", 150000, 5),
        ("Accountant", 125000, 3),
        ("Senior Accountant", 180000, 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Cris", "Carter", 1, NULL),
-       ("Ocho", "Cinco", 2, 1),
-       ("Randy", "Moss",3, ),
-       ("Jerry", "Rice", 4, 2),
+       ("Ocho", "Cinco", 2, 2),
+       ("Randy", "Moss",3, 5),
+       ("Jerry", "Rice", 4, 4),
        ("Steve", "Smith", 5, 3),
-       ("Calvin", "Johnson", 5, 2),
-       ("Larry", "Fitz", 6, 2);
+       ("Calvin", "Johnson", 6, 3),
+       ("Larry", "Fitz", 7, 5);
        
 
        
